fedd119dd5793ebf5e11afd87962fb0702f43757
modderme123 <modderme123@gmail.com>
2021-05-02 16:25:55 -0700

    generic

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,18 @@ import * as dat from "dat.gui";
 
 const gui = new dat.GUI();
 const params = {
-  threshold: 3.5,
-  scale: 55,
-  guesses: 50000,
+  n: 3,
+  l: 1,
+  m: 0,
+  threshold: 0.0001,
+  scale: 30,
+  guesses: 500000,
 };
-gui.add(params, "threshold", 0.01, 10).onChange(go);
-gui.add(params, "scale", 50, 100).onChange(go);
+gui.add(params, "n").onChange(go);
+gui.add(params, "l").onChange(go);
+gui.add(params, "m").onChange(go);
+gui.add(params, "threshold").onChange(go);
+gui.add(params, "scale").onChange(go);
 gui.add(params, "guesses", 50000, 200000, 1).onChange(go);
 
 const renderer = new THREE.WebGLRenderer();
@@ -17,36 +23,123 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 5, 3500);
-camera.position.z = 250;
+camera.position.z = 150;
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x050505);
-scene.fog = new THREE.Fog(0x050505, 2000, 3500);
+scene.fog = new THREE.Fog(0x050505, 150, 170);
 
 const geometry = new THREE.BufferGeometry();
+
+const a0 = 1; //5.29177210903e-11;
+
+function choosei(n: number, k: number) {
+  var result = 1;
+  for (var i = 1; i <= k; i++) {
+    result *= (n + 1 - i) / i;
+  }
+  return result;
+}
+function legendre(l: number, m: number) {
+  let prefix = (-1) ** m * 2 ** l;
+  let coeffs = [];
+  for (let k = m; k <= l; k++) {
+    coeffs.push((factorial(k) / factorial(k - m)) * choosei(l, k) * choosei((l + k - 1) / 2, l));
+  }
+  return (x: number) => {
+    let sum = 0;
+    for (let k = 0; k <= l - m; k++) {
+      sum += coeffs[k] * x ** k;
+    }
+    return prefix * (1 - x ** 2) ** (m / 2) * sum;
+  };
+}
+
+function sphericalHarmonics(l: number, m: number) {
+  const legendreOpt = legendre(l, m);
+  const prefix = (1 - (m & 1) * 2) * Math.sqrt(((2 * l + 1) * factorial(l - m)) / (4 * Math.PI * factorial(l + m)));
+
+  return (theta: number, phi: number) => {
+    return prefix * legendreOpt(Math.cos(theta)) * Math.cos(m * phi);
+  };
+}
+
+function laguerre(n: number, alpha: number) {
+  let coeffs = [];
+  for (let i = 0; i <= n; i++) {
+    coeffs.push(choosei(n + alpha, n - i) / factorial(i));
+  }
+  return (x: number) => {
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
+      sum += coeffs[i] * Math.pow(-x, i);
+    }
+    return sum;
+  };
+}
+
+function factorial(num: number) {
+  if (num <= 1) return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+
+function waveFn(n: number, l: number, m: number) {
+  const sqrtThing = Math.sqrt(((2 / (n * a0)) ** 3 * factorial(n - l - 1)) / (2 * n * factorial(l + n)));
+  const laguerreOpt = laguerre(n - l - 1, 2 * l + 1);
+  const sphericalOpt = sphericalHarmonics(l, m);
+  return (r: number, theta: number, phi: number) => {
+    let p = (2 * r) / (n * a0); //rho
+
+    return sqrtThing * Math.exp(-p / 2) * p ** l * laguerreOpt(p) * sphericalOpt(theta, phi);
+  };
+}
+
+function tests() {
+  console.log(legendre(2, 1)(-0.8), 1.44);
+  console.log(laguerre(3, 1)(6), 4);
+  console.log(sphericalHarmonics(0, 0)(0, 0), 1 / (2 * Math.sqrt(Math.PI)));
+
+  let x = (Math.random() - 0.5) * params.scale;
+  let y = (Math.random() - 0.5) * params.scale;
+  let z = (Math.random() - 0.5) * params.scale;
+
+  const r = Math.hypot(x, y, z);
+  const phi = Math.atan2(y, x);
+  const theta = Math.acos(z / r);
+
+  console.log(
+    waveFn(3, 1, 0)(r, theta, phi),
+    (2 * Math.sqrt(2) * r * Math.exp(-r / 3) * (2 - r / 3) * Math.cos(theta) * Math.sqrt(3 / (4 * Math.PI))) /
+      (27 * Math.sqrt(3))
+  );
+}
+// tests();
 
 function go() {
   const positions = [];
   const colors = [];
 
+  let wave = waveFn(params.n, params.l, params.m);
   for (let i = 0; i < params.guesses; i++) {
     let x = (Math.random() - 0.5) * params.scale;
     let y = (Math.random() - 0.5) * params.scale;
     let z = (Math.random() - 0.5) * params.scale;
 
     const r = Math.hypot(x, y, z);
-    const p = r / 3;
-    const radial = p * p * Math.exp(-p / 2);
-    const angular = (3 * z * z - r * r) / (r * r);
-    const wave = radial * angular;
+    const phi = Math.atan2(y, x);
+    const theta = Math.acos(z / r);
 
-    if (wave * wave < params.threshold)
-      continue;
+    const waveOut = wave(r, theta, phi);
 
-    positions.push(x, y, z);
+    if (waveOut * waveOut < params.threshold) continue;
 
-    if (wave > 0) colors.push(1, 0, 0);
+    positions.push((x / params.scale) * 50, (y / params.scale) * 50, (z / params.scale) * 50);
+
+    if (waveOut > 0) colors.push(1, 0, 0);
     else colors.push(0, 1, 0);
   }
 
