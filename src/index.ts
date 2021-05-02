99fb3c931ed227e64c3d74a09052dfba464747ce
modderme123 <modderme123@gmail.com>
2021-05-02 16:21:16 -0700

    tweak

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ const gui = new dat.GUI();
 const params = {
   threshold: 3.5,
   scale: 55,
+  guesses: 50000,
 };
 gui.add(params, "threshold", 0.01, 10).onChange(go);
 gui.add(params, "scale", 50, 100).onChange(go);
+gui.add(params, "guesses", 50000, 200000, 1).onChange(go);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,15 +24,13 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x050505);
 scene.fog = new THREE.Fog(0x050505, 2000, 3500);
 
-const particles = 50000;
-
 const geometry = new THREE.BufferGeometry();
 
 function go() {
   const positions = [];
   const colors = [];
 
-  for (let i = 0; i < particles; i++) {
+  for (let i = 0; i < params.guesses; i++) {
     let x = (Math.random() - 0.5) * params.scale;
     let y = (Math.random() - 0.5) * params.scale;
     let z = (Math.random() - 0.5) * params.scale;
@@ -41,10 +41,9 @@ function go() {
     const angular = (3 * z * z - r * r) / (r * r);
     const wave = radial * angular;
 
-    if (wave * wave < params.threshold) {
-      // i--;
+    if (wave * wave < params.threshold)
       continue;
-    }
+
     positions.push(x, y, z);
 
     if (wave > 0) colors.push(1, 0, 0);
@@ -57,6 +56,7 @@ function go() {
   geometry.computeBoundingSphere();
 }
 go();
+
 const material = new THREE.PointsMaterial({ size: 1, vertexColors: true });
 const points = new THREE.Points(geometry, material);
 scene.add(points);
